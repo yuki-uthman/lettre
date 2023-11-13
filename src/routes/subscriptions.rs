143ad3af1e9210a22502cf452a29c9c6bef43654
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-14 03:11:09 +0800

    fix: error type needs to implement ResponseError

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,8 +1,38 @@
 //! src/routes/subscriptions.rs
-use actix_web::{web, HttpResponse};
+use std::fmt::Display;
+
+use actix_web::http::{header::ContentType, StatusCode};
+use actix_web::{error::ResponseError, web, HttpResponse, Result};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+
+#[derive(Debug)]
+enum Error {
+    InternalError,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Error::InternalError => f.write_str("Internal Server Error"),
+        }
+    }
+}
+
+impl ResponseError for Error {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::html())
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match *self {
+            Error::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -11,7 +41,7 @@ pub struct Subscriber {
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<Subscriber>, pool: web::Data<PgPool>) -> Result<HttpResponse, HttpResponse> {
+pub async fn subscribe(form: web::Form<Subscriber>, pool: web::Data<PgPool>) -> Result<HttpResponse> {
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,7 +56,7 @@ pub async fn subscribe(form: web::Form<Subscriber>, pool: web::Data<PgPool>) -> 
     .await
     .map_err(|e| {
         println!("Failed to execute query: {}", e);
-        HttpResponse::InternalServerError().finish()
+        Error::InternalError
     })?;
 
     Ok(HttpResponse::Ok().finish())
