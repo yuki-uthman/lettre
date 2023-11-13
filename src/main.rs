9f5a10153339deab47e9332af26cd1778bb69e51
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-13 20:46:52 +0800

    feat: use config file in main
    It is possible to customize the port from the config file.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use lettre::configuration::get_configuration;
 use lettre::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let tcp_listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    let config = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", config.application_port);
+    let tcp_listener = TcpListener::bind(address).expect("Failed to bind port");
     run(tcp_listener)?.await
 }
