9887cec242c305dce5fe2c5a45fbabffa6ceaae9
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-14 02:32:24 +0800

    feat: impl subscribe

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,8 @@
 //! src/routes/subscriptions.rs
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -8,6 +11,23 @@ pub struct Subscriber {
     name: String,
 }
 
-pub async fn subscribe(_form: web::Form<Subscriber>) -> impl Responder {
-    HttpResponse::Ok()
+pub async fn subscribe(form: web::Form<Subscriber>, pool: web::Data<PgPool>) -> Result<HttpResponse, HttpResponse> {
+    sqlx::query!(
+        r#"
+    INSERT INTO subscriptions (id, email, name, subscribed_at)
+    VALUES ($1, $2, $3, $4)
+            "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.as_ref())
+    .await
+    .map_err(|e| {
+        println!("Failed to execute query: {}", e);
+        HttpResponse::InternalServerError().finish()
+    })?;
+
+    Ok(HttpResponse::Ok().finish())
 }
