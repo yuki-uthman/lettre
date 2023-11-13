7adc17639a147c32f9000ac2d200791b0d02ec76
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-13 15:08:02 +0800

    refactor: accept TcpListener rather than address
    This is needed to run the server on port 0 and get the assigned port back.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use lettre::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let tcp_listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000");
+    run(tcp_listener)?.await
 }
