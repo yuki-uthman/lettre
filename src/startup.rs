b229af76028a1e4f3d3d63bd1464f5c44d02920a
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-14 23:58:36 +0800

    refactor: change Logger to TracingLogger

--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,17 @@
 //! src/startup.rs
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(connection);
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
