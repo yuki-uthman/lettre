0b55a93897412f45240ad1f9e99ab980b057da70
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-14 22:11:39 +0800

    feat: allow log output customization
    get_subscriber accepts sink to specify where to output the log.
    Setting it to std::io::stdio prints to the terminal.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("lettre".into(), "info".into());
+    let subscriber = get_subscriber("lettre".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration.");
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,7 +2,7 @@ use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{fmt::MakeWriter, layer::SubscriberExt, EnvFilter, Registry};
 
 /// Compose multiple layers into a `tracing`'s subscriber.
 ///
@@ -10,10 +10,17 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 ///
 /// We are using `impl Subscriber` as return type to avoid having to spell out the actual
 /// type of the returned subscriber, which is indeed quite complex.
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sync + Send {
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Sync + Send
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
     Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
