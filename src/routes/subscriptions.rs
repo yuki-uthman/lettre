141311c2e7a3abba2b02a985390de5b13143cd15
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-14 22:39:15 +0800

    refactor: change to tracing::instrument macro
    The function body looks cleaner this way as it only includes the business logic.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,7 +4,6 @@ use actix_web::{error::ResponseError, web, HttpResponse, Result};
 use chrono::Utc;
 use sqlx::PgPool;
 use std::fmt::Display;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(Debug)]
@@ -41,30 +40,30 @@ pub struct Subscriber {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        email = %form.email,
+        name = %form.name
+    )
+)]
 pub async fn subscribe(
     form: web::Form<Subscriber>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse> {
-
-    insert_subscriber(&pool, &form).await.map_err(|e| {
-        tracing::error!("Failed to execute query:\n{:#?}", e);
-        Error::InternalError
-    })?;
+    insert_subscriber(&pool, &form)
+        .await
+        .map_err(|_| Error::InternalError)?;
 
     Ok(HttpResponse::Ok().finish())
 }
 
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
 async fn insert_subscriber(pool: &PgPool, form: &Subscriber) -> Result<(), sqlx::Error> {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "New subscriber!",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name= %form.name
-    );
-    let _request_span_guard = request_span.enter();
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -76,13 +75,7 @@ async fn insert_subscriber(pool: &PgPool, form: &Subscriber) -> Result<(), sqlx:
         Utc::now()
     )
     .execute(pool)
-    .instrument(query_span)
     .await?;
-
-    tracing::info!(
-        "{}: Saved new subscriber details in the database.",
-        request_id
-    );
 
     Ok(())
 }
