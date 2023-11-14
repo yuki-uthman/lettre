294325b76c31bf41ee016abe6b4c9ac67e7775c0
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-14 20:18:55 +0800

    refactor: extract into telemetry module

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,13 @@
 use lettre::configuration::get_configuration;
 use lettre::startup::run;
+use lettre::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-    // Make info the default log level
-    // to change the log level, use the RUST_LOG environment variable
-    // ie. RUST_LOG=debug cargo run
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "lettre".into(),
-        // Output the formatted spans to stdout.
-        std::io::stdout,
-    );
-    // The `with` method is provided by `SubscriberExt`, an extension
-    // trait for `Subscriber` exposed by `tracing_subscriber`
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    // `set_global_default` can be used by applications to specify
-    // what subscriber should be used to process spans.
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("lettre".into(), "info".into());
+    init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration.");
 
