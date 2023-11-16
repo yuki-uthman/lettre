bac7a3b0d6e15bdd2bbf3ed7d4dff0812d003b4c
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-17 01:10:57 +0800

    test: add integration tests for parsing errors

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -156,3 +156,72 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_name_is_invalid() {
+    // Arrange
+    let conn = setup_database().await;
+    let app_address = spawn_app(conn);
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        (
+            "name=bracket%7B&email=ursula_le_guin%40gmail.com",
+            "contains invalid char",
+        ),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_email_is_invalid() {
+    // Arrange
+    let conn = setup_database().await;
+    let app_address = spawn_app(conn);
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin&email=", "empty email"),
+        ("name=le%20guin&email=notanemail", "invalid email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
