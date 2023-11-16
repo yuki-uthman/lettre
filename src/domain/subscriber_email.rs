b556e03d958fe8d19c95d2f67a8a6744919ab0d1
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-17 01:03:04 +0800

    test(subscriber_email): add test for valid email

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -25,6 +25,8 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::*;
     use claims::assert_err;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::Fake;
 
     #[test]
     fn empty_string_is_rejected() {
@@ -42,5 +44,20 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
