94142c73097c9e2f9e451d42e3b528c6e72b1d99
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-16 23:52:45 +0800

    refactor: remove unnecessary error
    By returning the HttpResponse from subscribe and there is no need to return a Result.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,40 +1,10 @@
 //! src/routes/subscriptions.rs
 use crate::domain::Subscriber;
-use actix_web::http::{header::ContentType, StatusCode};
-use actix_web::{error::ResponseError, web, HttpResponse, Result};
+use actix_web::{web, HttpResponse, Result};
 use chrono::Utc;
 use sqlx::PgPool;
-use std::fmt::Display;
 use uuid::Uuid;
 
-#[derive(Debug)]
-enum Error {
-    InternalError,
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            Error::InternalError => f.write_str("Internal Server Error"),
-        }
-    }
-}
-
-impl ResponseError for Error {
-    fn error_response(&self) -> HttpResponse {
-        HttpResponse::build(self.status_code())
-            .insert_header(ContentType::html())
-            .body(self.to_string())
-    }
-
-    fn status_code(&self) -> StatusCode {
-        match *self {
-            Error::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
-        }
-    }
-}
-
-#[allow(dead_code)]
 #[derive(serde::Deserialize)]
 pub struct SubscriberForm {
     pub email: String,
@@ -50,15 +20,11 @@ pub struct SubscriberForm {
         subscriber_name = %form.name
     )
 )]
-pub async fn subscribe(
-    form: web::Form<SubscriberForm>,
-    pool: web::Data<PgPool>,
-) -> Result<HttpResponse> {
-    insert_subscriber(&pool, &form.into())
-        .await
-        .map_err(|_| Error::InternalError)?;
-
-    Ok(HttpResponse::Ok().finish())
+pub async fn subscribe(form: web::Form<SubscriberForm>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form.into()).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
 }
 
 #[tracing::instrument(
