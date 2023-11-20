4839c2cc0fa9ed642299d44e6e1b0b73967074ac
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-20 22:50:05 +0900

    test(person): add unit test

--- src/domain/person/mod.rs
+++ src/domain/person/mod.rs
@@ -42,3 +42,69 @@ impl TryFrom<SubscriberForm> for Person {
         })
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use colored::*;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::faker::name::en::Name as FakeName;
+    use fake::Fake;
+
+    macro_rules! matches {
+        ($expression:expr, $($pattern:tt)+) => {
+            match $expression {
+                $($pattern)+ => (),
+                ref e => {
+                    let right = stringify!($($pattern)+).green();
+                    let left = format!("{:?}", e).red();
+                    println!();
+                    println!("     {} =! {}", left, right);
+                    println!();
+                    panic!();
+                },
+            }
+        }
+    }
+
+    #[test]
+    fn valid_name_and_email() {
+        let name: String = FakeName().fake();
+        let email: String = SafeEmail().fake();
+
+        let result = Person::parse(name, email);
+        matches!(result, Ok(_));
+    }
+
+    #[test]
+    fn empty_name_is_rejected() {
+        let name = "".to_string();
+        let email = SafeEmail().fake();
+
+        matches!(Person::parse(name, email), Err(Error::NameError(_)));
+    }
+
+    #[test]
+    fn invalid_name_is_rejected() {
+        let name = "{hello}".to_string();
+        let email = SafeEmail().fake();
+
+        matches!(Person::parse(name, email), Err(Error::NameError(_)));
+    }
+
+    #[test]
+    fn empty_email_is_rejected() {
+        let name = FakeName().fake();
+        let email = "".to_string();
+
+        matches!(Person::parse(name, email), Err(Error::EmailError(_)));
+    }
+
+    #[test]
+    fn invalid_email_is_rejected() {
+        let name = FakeName().fake();
+        let email = "this is not an email".to_string();
+
+        matches!(Person::parse(name, email), Err(Error::EmailError(_)));
+    }
+}
