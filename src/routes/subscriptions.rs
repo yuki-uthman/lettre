9b832909a269a0c9b0c939af2e1f94f061ac29bb
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-11-23 06:08:53 +0900

    build: add status column to the db

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -52,8 +52,8 @@ pub async fn subscribe(
 async fn insert_subscriber(pool: &PgPool, form: &Person) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at)
-    VALUES ($1, $2, $3, $4)
+    INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+    VALUES ($1, $2, $3, $4, 'confirmed')
             "#,
         Uuid::new_v4(),
         form.email.as_ref(),
