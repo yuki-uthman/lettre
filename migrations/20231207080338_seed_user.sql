bcf1207d45b557f2d050d50695cb44149ddb83cd
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-12-07 17:22:59 +0900

    build: add admin user

---
+++ migrations/20231207080338_seed_user.sql
@@ -0,0 +1,10 @@
+-- Add migration script here
+
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'ddf8994f-d522-4659-8d02-c1d479057be6',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$yZNVT7plpze8oK4N1JqnfA$IyaI+7xrty7gUgfilqCZw09huT+RjeeSkJ23ODa073k'
+);
+
+
