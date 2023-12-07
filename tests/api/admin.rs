934feb9efef1deff1f53153fa94c0739ab1cc2dd
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-12-08 01:27:52 +0900

    test(login): check the redirect if not logged in

--- tests/api/admin.rs
+++ tests/api/admin.rs
@@ -13,3 +13,15 @@ async fn you_must_be_logged_in_to_access_the_admin_dashboard() {
     // Assert
     assert_is_redirect_to(&response, "/login");
 }
+
+#[tokio::test]
+async fn you_must_be_logged_in_to_change_password() {
+    // Arrange
+    let app = setup().await;
+
+    // Act
+    let response = app.get("/admin/password").await;
+
+    // Assert
+    assert_is_redirect_to(&response, "/login");
+}
