65d9e4431ca3ceb7103a67e6da5529bea92aa4d1
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-12-07 14:57:09 +0900

    test(admin): redirect to login page if not logged in

---
+++ tests/api/admin.rs
@@ -0,0 +1,15 @@
+//! src/tests/api/admin.rs
+
+use crate::helpers::{assert_is_redirect_to, setup};
+
+#[tokio::test]
+async fn you_must_be_logged_in_to_access_the_admin_dashboard() {
+    // Arrange
+    let app = setup().await;
+
+    // Act
+    let response = app.get("/admin/dashboard").await;
+
+    // Assert
+    assert_is_redirect_to(&response, "/login");
+}
