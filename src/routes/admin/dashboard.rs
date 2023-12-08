527db6fd7817777a79c8724fc7d42a0bd43a2dcf
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-12-08 14:07:11 +0900

    refactor(utils): move e500

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,17 +1,10 @@
 //! src/routes/admin/dashboard.rs
 
 use crate::session_state::TypedSession;
+use crate::utils::e500;
 use actix_web::{http::header::ContentType, web, HttpResponse};
 use anyhow::Context;
 use uuid::Uuid;
-
-// Return an opaque 500 while preserving the error's root cause for logging.
-fn e500<T>(e: T) -> actix_web::Error
-where
-    T: std::fmt::Debug + std::fmt::Display + 'static,
-{
-    actix_web::error::ErrorInternalServerError(e)
-}
 
 pub async fn admin_dashboard(
     session: TypedSession,
---
+++ src/utils.rs
@@ -0,0 +1,10 @@
+//! src/utils.rs
+
+// Return an opaque 500 while preserving the error's root cause for logging.
+pub fn e500<T>(e: T) -> actix_web::Error
+where
+    T: std::fmt::Debug + std::fmt::Display + 'static,
+{
+    actix_web::error::ErrorInternalServerError(e)
+}
+
